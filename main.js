10161eb3f86270209c72cc6bb9501b56921a3378
kasim_WX32 <45384577+MdKasimS@users.noreply.github.com>
2024-05-06 09:55:19 +0530

    JS file added for dynamic text in home section.

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:[".NET Fullstack Developer","Android Developer","Machine Enthusiast"],
+    typeSpeed:100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
