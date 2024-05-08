d91a2209932a2beb23644c4cba328f9c7679fd2c
kasim_WX32 <45384577+MdKasimS@users.noreply.github.com>
2024-05-08 22:00:57 +0530

    ABout section implemented. Starting Skills section

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings:["WPF .NET Developer","Android Developer","UIUX Designer"],
+    strings:["WPF .NET Developer","Android Developer","UI/UX Designer"],
     typeSpeed:100,
     backSpeed: 100,
     backDelay: 1000,
